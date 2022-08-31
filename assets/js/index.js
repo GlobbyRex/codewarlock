f2aed5f9c883bd50c2baa030fb999c56396072c4
GlobbyRex <rexnseobong@gmail.com>
2022-08-31 14:05:12 +0100

    Second Commit

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+   // Recap and re-intro.
+   // Display methods in JS.
+   // And input methods
+
+   // alert("This is displayed on a browser modal window!");
+   // console.log("Hello World");
+   // prompt("Enter your name");
+
+   // var identifier = value;
+   // var firstName = value;
+   //var , let and const
+   // var test = "Desmond";
+   // console.log(test)
+   let x = 9;
+   x = 90;
+   console.log(x);
